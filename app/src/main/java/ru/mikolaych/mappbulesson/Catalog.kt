d12585329a194bfe5ae205008294c333dc0798e3
Nikolay Kuzyakin <mikolaych@mail.ru>
2022-08-15 15:42:51 +0200

    15.08

--- app/src/main/java/ru/mikolaych/mappbulesson/Catalog.kt
+++ app/src/main/java/ru/mikolaych/mappbulesson/Catalog.kt
@@ -1,11 +1,12 @@
 package ru.mikolaych.mappbulesson
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import ru.mikolaych.mappbulesson.databinding.CatalogBinding
 
-class Catalog : AppCompatActivity() {
+class Catalog : AppCompatActivity(), View.OnClickListener {
     private var binding:CatalogBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +17,25 @@ class Catalog : AppCompatActivity() {
         binding?.catalog?.text = intent.getStringExtra("nameBut")
 
         //Изменение текста
-        binding?.editTextFromView?.setOnClickListener(View.OnClickListener {
-            binding?.catalog?.text = "Edited"
-        })
 
 
+        binding?.editTextFromView?.setOnClickListener(this)
+        binding?.editTextFromView2?.setOnClickListener(this)
+
+
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.editTextFromView -> {click(binding?.editTextFromView?.text, binding?.editTextFromView?.background)}
+            R.id.editTextFromView2 -> {click(binding?.editTextFromView2?.text, binding?.editTextFromView2?.background)}
+        }
+    }
+
+    private fun click(text: CharSequence?, background: Drawable?) {
+        binding?.catalog?.text = text
+        binding?.catalog?.background = background
 
     }
 }
